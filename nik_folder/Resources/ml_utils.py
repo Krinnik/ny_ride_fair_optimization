bd07ac378c52fece777e6d1e4b4a981de6673068
Krinnik <nikpsyllas@gmail.com>
2025-03-08 12:35:35 -0700

    more imputation work

--- nik_folder/Resources/ml_utils.py
+++ nik_folder/Resources/ml_utils.py
@@ -39,3 +39,30 @@ def impute_outliers_airport_fee(ny_taxi_2024_df):
     mask = pu_mask & fee_mask
     ny_taxi_2024_df.loc[mask, 'Airport_fee'] = 0
     return ny_taxi_2024_df
+
+def impute_na_ratecodeID(ny_taxi_2024_df):
+    def_mask = (ny_taxi_2024_df['PULocationID'] != ny_taxi_2024_df['PULocationID'].isin([np.isnan, 1, 132, 250, 265])) | (
+        ny_taxi_2024_df['DOLocationID'] != ny_taxi_2024_df['DOLocationID'].isin([np.isnan, 1, 132, 250, 265])
+    )
+    rate_mask = (ny_taxi_2024_df['RatecodeID'] > 6) | np.isnan(ny_taxi_2024_df['RatecodeID'])
+    mask = def_mask & rate_mask
+    ny_taxi_2024_df.loc[mask, ['RatecodeID']] = 1
+
+
+def impute_ratecodeIDs(ny_taxi_2024_df):
+    mask = (ny_taxi_2024_df['PULocationID'] == 1) | (ny_taxi_2024_df['DOLocationID'] == 1)
+    mask_2 = (ny_taxi_2024_df['PULocationID'] == 132) | (ny_taxi_2024_df['DOLocationID'] == 132)
+    mask_3 = (ny_taxi_2024_df['PULocationID'] == 250) | (ny_taxi_2024_df['DOLocationID'] == 250) | (
+        ny_taxi_2024_df['PULocationID'] == 265) | (ny_taxi_2024_df['DOLocationID'] == 265)
+    ny_taxi_2024_df.loc[mask, ['RatecodeID']] = 3
+    ny_taxi_2024_df.loc[mask_2, ['RatecodeID']] = 2
+    ny_taxi_2024_df.loc[mask_3, ['RatecodeID']] = 4
+    return ny_taxi_2024_df
+
+def impute_negatives(ny_taxi_2024_df):
+    negatives = ['fare_amount', 'extra', 'tip_amount', 'tolls_amount', 'mta_tax', 'improvement_surcharge', 'total_amount', 'congestion_surcharge']
+    mask = (ny_taxi_2024_df[negatives] < 0).any(axis=1)
+    mask_2 = (np.isnan(ny_taxi_2024_df['congestion_surcharge']))
+    ny_taxi_2024_df.loc[mask, negatives] = ny_taxi_2024_df.loc[mask, negatives].abs()
+    ny_taxi_2024_df.loc[mask_2, ['congestion_surcharge']] = 0
+    return ny_taxi_2024_df
